6db6094f9afe0efba4ef7f94fbf88a8a30a9b18e
Корнилов <rfmatrehska@gmail.com>
2017-11-16 17:56:14 +0500

    Json ответы от ApiController с данными о заказах на покупку и заказах на продажу

---
+++ GingerbreadExchange/Controllers/ApiController.cs
@@ -0,0 +1,29 @@
+﻿using GingerbreadExchange.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GingerbreadExchange.Controllers
+{
+    public class ApiController : Controller
+    {
+        API api = new API();
+
+        [HttpGet]
+        public JsonResult GetBuyOrders(string name)
+        {
+            var jsondata = api.GetBuyOrdersModel();
+            return Json(jsondata, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public JsonResult GetSellOrders(string name)
+        {
+            var jsondata = api.GetSellOrdersModel();
+            return Json(jsondata, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
